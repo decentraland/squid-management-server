112e54769aa0c37daf29a34a27e2e2c98165f77a
Juanma Hidalgo <juanma06@gmail.com>
2024-11-14 14:34:19 +0100

    fix: order import for eslint

--- src/components.ts
+++ src/components.ts
@@ -1,13 +1,13 @@
 import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
 import { createServerComponent, createStatusCheckComponent } from '@well-known-components/http-server'
 import { createLogComponent } from '@well-known-components/logger'
-import { metricDeclarations } from './metrics'
 import { createMetricsComponent } from '@well-known-components/metrics'
 import { createTracerComponent } from '@well-known-components/tracer-component'
-import { AppComponents, GlobalContext } from './types'
-import { createSubsquidComponent } from './ports/squids/component'
-import { createPgComponent } from './ports/db/component'
 import { createFetchComponent } from './adapters/fetch'
+import { metricDeclarations } from './metrics'
+import { createPgComponent } from './ports/db/component'
+import { createSubsquidComponent } from './ports/squids/component'
+import { AppComponents, GlobalContext } from './types'
 
 // Initialize all the components of the app
 export async function initComponents(): Promise<AppComponents> {
